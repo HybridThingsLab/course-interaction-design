57a4a83201edac19637ea9ffb79c49889332086f
Andreas Muxel <login@andreasmuxel.com>
2025-04-22 16:11:12 +0200

    update p5js and ml5js examples to latest versions

--- Block_II/p5js/examples/07_poseNet/sketch.js
+++ Block_II/p5js/examples/07_poseNet/sketch.js
@@ -1,100 +1,74 @@
-// webcam
-let video;
+/*
+ * 👋 Hello! This is an ml5.js example made and shared with ❤️.
+ * Learn more about the ml5.js project: https://ml5js.org/
+ * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+ *
+ * This example demonstrates drawing skeletons on poses for the MoveNet model.
+ */
 
-// poseNet
-let poseNet;
+let video;
+let bodyPose;
 let poses = [];
+let connections;
+
+function preload() {
+  // Load the bodyPose model
+  bodyPose = ml5.bodyPose();
+}
 
 function setup() {
-
-  // canvas
   const canvas = createCanvas(640, 480);
   canvas.parent('canvas');
 
-  // init webcam
+  // Create the video and hide it
   video = createCapture(VIDEO);
-  video.size(width, height);
+  video.size(640, 480);
   video.hide();
 
-  // init poseNet Model, see https://github.com/tensorflow/tfjs-models/tree/master/posenet
-  // flip horizontal und just pose of one person
-  poseNet = ml5.poseNet(video, {
-    flipHorizontal: true,
-    detectionType: 'single'
-  }, modelReady);
-  select('#output').html('... loading model');
-
-  // detect if new pose detected and call 'gotResultModel'
-  poseNet.on('pose', gotResultsModel);
-
+  // Start detecting poses in the webcam video
+  bodyPose.detectStart(video, gotPoses);
+  // Get the skeleton connection information
+  connections = bodyPose.getSkeleton();
 }
 
 function draw() {
-
-  // clear background
-  background(0);
-
-  // show video (flipped)
-  push();
-  translate(width, 0);
-  scale(-1, 1);
+  // Draw the webcam video
   image(video, 0, 0, width, height);
-  pop();
 
-  // show results of poseNet
-  drawKeypoints();
-  drawSkeleton();
-
-}
-
-
-// model ready
-function modelReady() {
-  select('#output').html('model loaded');
-}
-
-// results of current model (p.ex. PoseNet, handpose, facemesh...)
-function gotResultsModel(result) {
-  poses = result;
-
-  // have a detailed look in your console
-  //console.log(result)
-}
-
-///////////////////////////
-// Visualization PoseNet //
-///////////////////////////
-
-// draw ellipses over the detected keypoints
-function drawKeypoints() {
-  // loop through all the poses detected
+  // Draw the skeleton connections
   for (let i = 0; i < poses.length; i++) {
-    // for each pose detected, loop through all the keypoints
-    let pose = poses[i].pose;
+    let pose = poses[i];
+    for (let j = 0; j < connections.length; j++) {
+      let pointAIndex = connections[j][0];
+      let pointBIndex = connections[j][1];
+      let pointA = pose.keypoints[pointAIndex];
+      let pointB = pose.keypoints[pointBIndex];
+      // Only draw a line if both points are confident enough
+      if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+      }
+    }
+  }
+
+  // Draw all the tracked landmark points
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
     for (let j = 0; j < pose.keypoints.length; j++) {
-      // a keypoint is an object describing a body part (like rightArm or leftShoulder)
       let keypoint = pose.keypoints[j];
-      // only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
+      // Only draw a circle if the keypoint's confidence is bigger than 0.1
+      if (keypoint.confidence > 0.1) {
         fill(0, 255, 0);
         noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+        circle(keypoint.x, keypoint.y, 10);
       }
     }
   }
 }
 
-// draw the skeletons
-function drawSkeleton() {
-  // loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i++) {
-    let skeleton = poses[i].skeleton;
-    // for every skeleton, loop through all body connections
-    for (let j = 0; j < skeleton.length; j++) {
-      let partA = skeleton[j][0];
-      let partB = skeleton[j][1];
-      stroke(0, 255, 0);
-      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-    }
-  }
+// Callback function for when bodyPose outputs data
+function gotPoses(results) {
+  // Save the output to the poses variable
+  poses = results;
 }
--- Block_II/p5js/examples/08_handpose/sketch.js
+++ Block_II/p5js/examples/08_handpose/sketch.js
@@ -1,127 +1,48 @@
-// webcam
+ /* 👋 Hello! This is an ml5.js example made and shared with ❤️.
+ * Learn more about the ml5.js project: https://ml5js.org/
+ * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+ *
+ * This example demonstrates hand tracking on live video through ml5.handPose.
+ */
+
+let handPose;
 let video;
+let hands = [];
 
-// handpose
-let handpose;
-let poses = [];
-
+function preload() {
+  // Load the handPose model
+  handPose = ml5.handPose();
+}
 
 function setup() {
-
-  // canvas
   const canvas = createCanvas(640, 480);
   canvas.parent('canvas');
-
-  // init webcam
+  // Create the webcam video and hide it
   video = createCapture(VIDEO);
-  video.size(width, height);
+  video.size(640, 480);
   video.hide();
-
-  // init handpose, see also https://google.github.io/mediapipe/solutions/hands.html
-
-  // options
-  const options = {
-    flipHorizontal: true, // boolean value for if the video should be flipped, defaults to false
-    maxContinuousChecks: Infinity, // How many frames to go without running the bounding box detector. Defaults to infinity, but try a lower value if the detector is consistently producing bad predictions.
-    detectionConfidence: 0.8, // Threshold for discarding a prediction. Defaults to 0.8.
-    scoreThreshold: 0.75, // A threshold for removing multiple (likely duplicate) detections based on a "non-maximum suppression" algorithm. Defaults to 0.75
-    iouThreshold: 0.3, // A float representing the threshold for deciding whether boxes overlap too much in non-maximum suppression. Must be between [0, 1]. Defaults to 0.3.
-  }
-  handpose = ml5.handpose(video, options, modelReady);
-
-  select('#output').html('... loading model');
-
-  // detect if new pose detected and call 'gotResultModel'
-  handpose.on('predict', gotResultsModel);
-
-  // Hide the video element, and just show the canvas
-  video.hide();
-
+  // start detecting hands from the webcam video
+  handPose.detectStart(video, gotHands);
 }
-
 
 function draw() {
-  // clear background
-  background(0);
-
-  // show video (flipped)
-  push();
-  translate(width, 0);
-  scale(-1, 1);
+  // Draw the webcam video
   image(video, 0, 0, width, height);
-  pop();
 
-  // show results of handpose
-  drawKeypoints();
-  drawSkeleton();
-
-}
-
-
-// model ready
-function modelReady() {
-  select('#output').html('model loaded');
-}
-
-// results of current model (p.ex. PoseNet, handpose, facemesh...)
-function gotResultsModel(result) {
-  poses = result;
-  // just update optimized input data if new input data available
-  if (poses.length > 0) {
-    optimizedInputData = poses[0].landmarks;
-    // console.log(optimizedInputData);
-  }
-}
-
-////////////////////////////
-// Visualization handpose //
-////////////////////////////
-
-// draw ellipses over the detected keypoints
-function drawKeypoints() {
-  for (let i = 0; i < poses.length; i++) {
-    const pose = poses[i];
-
-    // have a detailed look in your console
-    // console.log(pose); 
-
-    for (let j = 0; j < pose.landmarks.length; j += 1) {
-      const keypoint = pose.landmarks[j];
+  // Draw all the tracked hand points
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    for (let j = 0; j < hand.keypoints.length; j++) {
+      let keypoint = hand.keypoints[j];
       fill(0, 255, 0);
       noStroke();
-      ellipse(keypoint[0], keypoint[1], 10, 10);
+      circle(keypoint.x, keypoint.y, 10);
     }
   }
 }
 
-// draw the skeletons
-function drawSkeleton() {
-
-  for (let i = 0; i < poses.length; i++) {
-    //onst pose = poses[i];
-    let annotations = poses[0].annotations;
-    stroke(0, 255, 0);
-    for (let j = 0; j < annotations.thumb.length - 1; j++) {
-      line(annotations.thumb[j][0], annotations.thumb[j][1], annotations.thumb[j + 1][0], annotations.thumb[j + 1][1]);
-    }
-    for (let j = 0; j < annotations.indexFinger.length - 1; j++) {
-      line(annotations.indexFinger[j][0], annotations.indexFinger[j][1], annotations.indexFinger[j + 1][0], annotations.indexFinger[j + 1][1]);
-    }
-    for (let j = 0; j < annotations.middleFinger.length - 1; j++) {
-      line(annotations.middleFinger[j][0], annotations.middleFinger[j][1], annotations.middleFinger[j + 1][0], annotations.middleFinger[j + 1][1]);
-    }
-    for (let j = 0; j < annotations.ringFinger.length - 1; j++) {
-      line(annotations.ringFinger[j][0], annotations.ringFinger[j][1], annotations.ringFinger[j + 1][0], annotations.ringFinger[j + 1][1]);
-    }
-    for (let j = 0; j < annotations.pinky.length - 1; j++) {
-      line(annotations.pinky[j][0], annotations.pinky[j][1], annotations.pinky[j + 1][0], annotations.pinky[j + 1][1]);
-    }
-
-    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.thumb[0][0], annotations.thumb[0][1]);
-    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.indexFinger[0][0], annotations.indexFinger[0][1]);
-    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.middleFinger[0][0], annotations.middleFinger[0][1]);
-    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.ringFinger[0][0], annotations.ringFinger[0][1]);
-    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.pinky[0][0], annotations.pinky[0][1]);
-  }
-
+// Callback function for when handPose outputs data
+function gotHands(results) {
+  // save the output to the hands variable
+  hands = results;
 }
